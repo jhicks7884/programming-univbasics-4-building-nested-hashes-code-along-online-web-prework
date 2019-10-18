0b930c66fa28ca2e3189b21231a7283b6b8da386
Jeshua Hicks <jhicks7884@outlook.com>
2019-10-18 16:41:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,10 @@ def third_challenge
       },
       :hero => {name: "Romeo", age: "15", status: "alive"        #your key/value pairs here
         },
-      :hero_friends => []
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}    
+        ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"
@@ -16,7 +19,10 @@ def third_challenge
         },
       :heroine => {name: "Juliet", age: "15", status: "alive"
                 },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}  
+        ]
    }
   }
 
